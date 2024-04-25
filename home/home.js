7be8bc25441e14888e39746908766450d3003d1e
yosrjdly <yosrjdly@gmail.com>
2024-04-25 16:19:02 +0200

    lkfk

--- home/home.js
+++ home/home.js
@@ -10,39 +10,39 @@ function makeApp(nameProj) {
 
 $("#select-movie").on("click", function () {
 
+});
+
+var filteredMakeList =[]
+ 
+function search(arr){
+     filteredMakeList = filter(arr,function(ele){
+         return ele===movieList(ele)
+     })
+ }
+
+
+function displayAll() {
+    this.movieList.map(function (ele) {
+        $("body").append(`
+            ${ele.movieName}`)
     })
+}
 
-    // var search = document.getElementsByClassName("search")
-    // $("search").on('click',function(string){
-    //     $(input-value.val()===string){
-    //         return string
-    //     }
-    // })
+function addMovie(movieName, Category, releaseDate, image, rating) {
+    this.movieList.push({
+        movieName: movieName,
+        Category: Category,
+        releaseDate: releaseDate,
+        image: image,
+        rating: rating
+    });
+}
 
-    function addMovie(movieName,Category,releaseDate) {
-        this.movieList.push({
-            movieName:movieName,
-            Category:Category,
-            releaseDate:releaseDate
-        });
-    }
-    var Movie1 = makeApp('6 Hours Away',"Crime","2024-04-19",)
-    var Movie2 = makeApp("Villians Incorported",'Comedy',"2024-04-19")
-    var Movie3 = makeApp("The Grey Man","Action","2022-07-22")
-    var Movie4 = makeApp("Harry Potter:Hogwarts Mystery","Sci-Fiction","2024-04-25")
-    console.log("hello")
-    function displayAll() {
-        this.movieList.map(function (ele) {
-            $("body").append(`
-            ${ele.namee}`)
-        })
-    }
-
-
-
-    a.addMovie('azf')
-    a.addMovie('azf')
-    a.addMovie('azf')
+var Web = makeApp('Te3ba la3bed')
+Web.Movie1 = addMovie('6 Hours Away', "Crime", "2024-04-19",)
+Web.Movie2 = addMovie("Villians Incorported", 'Comedy', "2024-04-19")
+Web.Movie3 = addMovie("The Grey Man", "Action", "2022-07-22")
+Web.Movie4 = addMovie("Harry Potter:Hogwarts Mystery", "Sci-Fiction", "2024-04-25")
 
 a.displayAll()
 
