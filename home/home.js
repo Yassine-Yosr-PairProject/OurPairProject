5c7e9724eb9251c1a2485560f45c5d508cfcfd4d
Da7eddine <yassineboudidah@gmail.com>
2024-04-25 22:39:01 +0200

    final js

--- home/home.js
+++ home/home.js
@@ -4,52 +4,107 @@ function makeApp(nameProj) {
         nameProj: nameProj,
         movieList: [],
         addMovie: addMovie,
-        displayAll: displayAll
+        displayAll: displayAll,
+        changeRating: rate,
+    }
+}
+var Web = makeApp('Te3ba la3bed')
+Web.addMovie('6 Hours Away', "Crime", "2024-04-19", "https://cdn.europosters.eu/image/750/wall-murals/harry-potter-hogwarts-i105088.jpg", 4)
+Web.addMovie("Damaged", "Crime", "2024-04-12", "https://cdn.europosters.eu/image/750/wall-murals/harry-potter-hogwarts-i105088.jpg", 3)
+Web.addMovie("Villians Incorported", 'Comedy', "2024-04-19", "https://cdn.europosters.eu/image/750/wall-murals/harry-potter-hogwarts-i105088.jpg", 3)
+Web.addMovie("Love,Divided", "Comedy", "2024-04-12", "https://cdn.europosters.eu/image/750/wall-murals/harry-potter-hogwarts-i105088.jpg", 2)
+Web.addMovie("The Grey Man", "Action", "2022-07-22", "https://cdn.europosters.eu/image/750/wall-murals/harry-potter-hogwarts-i105088.jpg", 4)
+Web.addMovie("Civil War", "Action", "2024-04-10", "https://cdn.europosters.eu/image/750/wall-murals/harry-potter-hogwarts-i105088.jpg", 3)
+Web.addMovie("The Shawshank Redemption", "Drama", "1994-09-23", "https://cdn.europosters.eu/image/750/wall-murals/harry-potter-hogwarts-i105088.jpg", 5)
+Web.addMovie("Scoop", "Drama", "2024-04-05", "https://cdn.europosters.eu/image/750/wall-murals/harry-potter-hogwarts-i105088.jpg", 3)
+Web.addMovie("Harry Potter:Hogwarts Mystery", "Sci-Fiction", "2024-04-25", "https://cdn.europosters.eu/image/750/wall-murals/harry-potter-hogwarts-i105088.jpg", 5)
+Web.addMovie("Divergent", "Sci-Fiction", "2014-03-14", "https://cdn.europosters.eu/image/750/wall-murals/harry-potter-hogwarts-i105088.jpg", 5)
+
+$("#select-movie").on("click", function (e) {
+    search($("#input-value").val(),$("#select-cat").val())
+});
+
+function search(keyword,cat) {
+    var arr = Web.movieList
+    var filteredMakeList = filter(arr, function (ele) {
+        return ele.movieName.toLowerCase().includes(keyword.toLowerCase()) 
+        && (ele.Category === cat || cat === "all")
+    })
+    Web.displayAll(filteredMakeList)
+}
+
+
+
+function displayAll(arr) {
+    $(".movies").html("")
+    $(".detail-app").hide()
+
+    for (let i = 0; i < arr.length; i++) {
+        $(".movies").append(`
+            <div>
+            <img src="${arr[i].image}" width="100px" height="100px">
+            <h4>${arr[i].movieName}</h4>
+            <span class="material-symbols-outlined fav-icon">
+             favorite
+            </span>
+            <button class="detail-btn" id="${i}">
+                detail
+            </button>
+            </div>`
+        )
+    }
+    handlFavIcon()
+    handleDetail()
+}
+function handlFavIcon() {
+    var favBtns = $(".fav-icon")
+    for (var i = 0; i < favBtns.length; i++) {
+        favBtns[i].addEventListener("click", function (e) {
+            e.target.style = "background:red;"
+        })
     }
 }
 
-$("#select-movie").on("click", function () {
-
-});
-
-var filteredMakeList =[]
- 
-function search(arr){
-     filteredMakeList = filter(arr,function(ele){
-         return ele===movieList(ele)
-     })
- }
-
-
-function displayAll() {
-    this.movieList.map(function (ele) {
-        $(".movies").append(`
-            <div>
-                <img src="${ele.image}" width="100px" height="100px">
-                <h4>${ele.movieName}</h4>
-                <p>${ele.releaseDate}</p>
-            </div>`
-        )
+function rate() {
+    $(".rate-input").on("keyup", function () {
+        var newRate = $(".rate-input").val()
+        var idx = $(".rate-input").attr("id")
+        Web.movieList[idx].rating = newRate
+        Web.displayAll(Web.movieList)
     })
 }
+function handleDetail() {
+    var arr = Web.movieList
+    var detailBtns = $(".detail-btn")
+    for (let i = 0; i < detailBtns.length; i++) {
+        detailBtns[i].addEventListener("click", function (e) {
+            var x = e.target.getAttribute("id")
+            $(".movies").hide()
+            $("body").append(`<div class="detail-app">
+            <img src="${arr[x].image}" width="100px" height="100px">
+            <h4>${arr[x].movieName}</h4>
+            <p>${arr[x].releaseDate}</p>
+            <p>${arr[x].rating}</p>
+            <a href="home.html">Home</a>
+            </div>`)
+            rate()
+        })
+        // <p>${arr[x].movieDesc}</p>
+    }
+}
 
-function addMovie(movieName, Category, releaseDate, image, rating) {
+function addMovie(movieName, Category, releaseDate, image, rating, Desc) {
     this.movieList.push({
         movieName: movieName,
         Category: Category,
         releaseDate: releaseDate,
         image: image,
-        rating: rating
+        rating: rating,
+        Desc: Desc
     });
 }
 
-var Web = makeApp('Te3ba la3bed')
-Web.addMovie('6 Hours Away', "Crime", "2024-04-19","https://static.wixstatic.com/media/af1176_cd1cc93602cf465fa5e78b3146f4c505~mv2.jpg/v1/fill/w_560,h_840,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/IMG_6044_JPG.jpg")
-Web.addMovie("Villians Incorported", 'Comedy', "2024-04-19","https://static.wixstatic.com/media/af1176_cd1cc93602cf465fa5e78b3146f4c505~mv2.jpg/v1/fill/w_560,h_840,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/IMG_6044_JPG.jpg")
-Web.addMovie("The Grey Man", "Action", "2022-07-22","https://static.wixstatic.com/media/af1176_cd1cc93602cf465fa5e78b3146f4c505~mv2.jpg/v1/fill/w_560,h_840,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/IMG_6044_JPG.jpg")
-Web.addMovie("Harry Potter:Hogwarts Mystery", "Sci-Fiction", "2024-04-25","https://static.wixstatic.com/media/af1176_cd1cc93602cf465fa5e78b3146f4c505~mv2.jpg/v1/fill/w_560,h_840,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/IMG_6044_JPG.jpg")
-console.log(Web)
-Web.displayAll()
+Web.displayAll(Web.movieList)
 
 
 
